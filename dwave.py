cd3adc0159c6dac138a3a5d778b2399538dd12c8
Anonymous <372035319@qq.com>
2021-06-10 18:26:58 -0500

    2

--- dwave.py
+++ dwave.py
@@ -15,15 +15,10 @@ class TimeBlock(nn.Module):
         super(TimeBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
         self.conv2 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
-        self.conv3 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
-
     def forward(self, X):
 
         X = X.permute(0, 3, 1, 2)
         temp = self.conv1(X) * torch.sigmoid(self.conv2(X))
-        rX=torch.flip(X, [3])
-        rtemp=self.conv1(rX) * torch.sigmoid(self.conv2(rX))
-        temp=torch.cat((temp, rtemp), 3)
         out = temp.permute(0, 2, 3, 1)
 
         return out
@@ -33,10 +28,8 @@ class PCNLayer(nn.Module):
         super(PCNLayer, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
         self.conv2 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
-        self.conv3 = nn.Conv2d(in_channels, out_channels, (1, kernel_size),dilation=step)
-    def forward(self, x,s2rDic,trajDic):
+    def forward(self, x,r2sDic,s2rDic,trajDic):
 
-        #print(x.shape)
         keys=[*s2rDic]
         h=[]
         for s in s2rDic:
@@ -48,9 +41,6 @@ class PCNLayer(nn.Module):
             temp1=torch.stack(temp1)
             temp1=temp1.permute(1, 3, 2, 0)
             temp2 = self.conv1(temp1) * torch.sigmoid(self.conv2(temp1))
-            rtemp1=torch.flip(temp1, [3])
-            rtemp2=self.conv1(rtemp1) * torch.sigmoid(self.conv2(rtemp1))
-            temp2=torch.cat((temp2, rtemp2), 3)
             temp.append(temp2)            
           temp=torch.stack(temp)
           temp=temp.mean(0)
@@ -70,11 +60,14 @@ class PCNBlock(nn.Module):
         self.PCNLayer = PCNLayer(in_channels=out_channels,
                                    out_channels=out_channels)
 
-    def forward(self, X,s2rDic,trajDic):
+    def forward(self, X,X_daily,X_weekly,X_coarse,r2sDic,s2rDic,trajDic):
 
         x = self.temporal1(X)
+        x_daily = self.temporal1(X_daily)
+        x_weekly = self.temporal1(X_weekly)
+        x=torch.cat((x, x_daily,x_weekly), 2)
         x=x.permute(1, 0, 2, 3)
-        output=self.PCNLayer(x,s2rDic,trajDic)
+        output=self.PCNLayer(x,r2sDic,s2rDic,trajDic)
         return output
 
 
@@ -86,18 +79,31 @@ class PCN(nn.Module):
         super(PCN, self).__init__()
         self.block1 = PCNBlock(in_channels=num_features,
                                out_channels=16,num_nodes=num_nodes)
-        self.fully = nn.Linear((num_timesteps_input - 2 * 2)*2*3*2 * 16,
+        self.fully = nn.Linear((num_timesteps_input - 2 * 2+2+8)*3 * 16,
                                num_timesteps_output,bias = True)
 
 
-    def forward(self,X,r2sDic,s2rDic,trajDic):
+    def forward(self,X,X_daily,X_weekly,X_coarse,r2sDic,s2rDic,trajDic):
 
         temp=[]
+        tempD=[]
+        tempW=[]
+        tempC=[]
         for seg in s2rDic:
           temp.append(X[:,s2rDic[seg],:,:])
+          tempD.append(X_daily[:,s2rDic[seg],:,:])
+          tempW.append(X_weekly[:,s2rDic[seg],:,:])
+          tempC.append(X_coarse[:,s2rDic[seg],:,:])
+
         X=torch.stack(temp)
+        X_daily=torch.stack(tempD)
+        X_weekly=torch.stack(tempW)
+        X_coarse=torch.stack(tempC)
         X=X.permute((1,0,2,3))
-        out1 = self.block1(X,s2rDic,trajDic)
+        X_daily=X_daily.permute((1,0,2,3))
+        X_weekly=X_weekly.permute((1,0,2,3))
+        X_coarse=X_coarse.permute((1,0,2,3))
+        out1 = self.block1(X,X_daily,X_weekly,X_coarse,r2sDic,s2rDic,trajDic)
         out3=out1
         out4 = self.fully(out3.reshape((out3.shape[0], out3.shape[1], -1)))
         temp2=[]
@@ -110,5 +116,4 @@ class PCN(nn.Module):
           temp2.append(temp_new)
         out4=torch.stack(temp2).permute((1,0,2))
         return out4
-
 
